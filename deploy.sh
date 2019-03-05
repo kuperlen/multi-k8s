314b019710fb2f60e4924fdb3da805ee2545fa29
kuperlen <lkuperman@statestreet.com>
2019-03-05 12:10:32 -0500

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t kuperlen/multi-client:latest -t kuperlen/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t kuperlen/multi-server:latest -t kuperlen/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t kuperlen/multi-worker:latest -t kuperlen/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push kuperlen/multi-client:$SHA
+docker push kuperlen/multi-server:$SHA
+docker push kuperlen/multi-worker:$SHA
+
+docker push kuperlen/multi-client:latest
+docker push kuperlen/multi-server:latest
+docker push kuperlen/multi-worker:latest
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=kuperlen/multi-server:$SHA
+kubectl set image deployments/client-deployment client=kuperlen/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=kuperlen/multi-server:$SHA
