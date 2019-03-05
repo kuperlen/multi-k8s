6beb81c315f06e3931b758acf7c7829ff262bd64
kuperlen <lkuperman@statestreet.com>
2019-03-05 16:13:09 -0500

    fixed a typo

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
+  
 docker build -t kuperlen/multi-client:latest -t kuperlen/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t kuperlen/multi-server:latest -t kuperlen/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t kuperlen/multi-worker:latest -t kuperlen/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push kuperlen/multi-client:$SHA
-docker push kuperlen/multi-server:$SHA
-docker push kuperlen/multi-worker:$SHA
-
 docker push kuperlen/multi-client:latest
 docker push kuperlen/multi-server:latest
 docker push kuperlen/multi-worker:latest
+
+docker push kuperlen/multi-client:$SHA
+docker push kuperlen/multi-server:$SHA
+docker push kuperlen/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=kuperlen/multi-server:$SHA
